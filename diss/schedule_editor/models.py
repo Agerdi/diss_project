47ee7b1a100b060d71f0a0359eada5c2c622a527
Volodya Leveryev <volodya.leveryev@gmail.com>
2018-03-03 02:44:54 +0900

    Обновление до Django 2.0.2

--- diss/schedule_editor/models.py
+++ diss/schedule_editor/models.py
@@ -2,70 +2,72 @@ from django.db import models
 
 
 class Discipline(models.Model):
+    name = models.CharField('Наименование', max_length=100)
+
     class Meta:
         verbose_name = 'Дисциплина'
         verbose_name_plural = 'Дисциплины'
         ordering = ['name']
-
-    name = models.CharField('Наименование', max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class Group(models.Model):
+    name = models.CharField('Наименование', max_length=30)
+    disciplines = models.ManyToManyField('Discipline', verbose_name='Дисциплины')
+
     class Meta:
         verbose_name = 'Группа'
         verbose_name_plural = 'Группы'
 
-    name = models.CharField('Наименование', max_length=30)
-    disciplines = models.ManyToManyField('Discipline', verbose_name='Дисциплины')
-
 
 class Student(models.Model):
+    fullname = models.CharField('Полное имя', max_length=50)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    disciplines = models.ManyToManyField('Discipline', verbose_name='Дисциплины')
+
     class Meta:
         verbose_name = 'Студент'
         verbose_name_plural = 'Студенты'
 
-    fullname = models.CharField('Полное имя', max_length=50)
-    group = models.ForeignKey('Group')
-    disciplines = models.ManyToManyField('Discipline', verbose_name='Дисциплины')
-
 
 class Classroom(models.Model):
+    number = models.CharField('Номер', max_length=50)
+    build = models.CharField('Здание', max_length=50)
+
     class Meta:
         verbose_name = 'Аудитория'
         verbose_name_plural = 'Аудитории'
         ordering = ['number']
 
-    number = models.CharField('Номер', max_length=50)
-    build = models.CharField('Здание', max_length=50)
-
     def __str__(self):
         return self.number + ' ' + self.build
 
+
 class Teacher(models.Model):
+    fullname = models.CharField('Полное имя', max_length=50)
+
     class Meta:
         verbose_name = 'Преподаватель'
         verbose_name_plural = 'Преподаватели'
         # ordering = ['name']
-
-    fullname = models.CharField('Полное имя', max_length=50)
 
     def __str__(self):
         return self.fullname
 
 
 class Event(models.Model):
-    class Meta:
-        verbose_name = 'Событие'
-        verbose_name_plural = 'События'
-
-    discipline = models.ForeignKey('Discipline', verbose_name='Дисциплина', blank=True, null=True)
-    classroom = models.ForeignKey('Classroom', verbose_name='Аудитория')
-    teacher = models.ForeignKey('Teacher', verbose_name='Преподаватель', blank=True, null=True)
+    discipline = models.ForeignKey('Discipline', on_delete=models.CASCADE, blank=True, null=True,
+                                   verbose_name='Дисциплина')
+    classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE, verbose_name='Аудитория')
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, blank=True, null=True,
+                                verbose_name='Преподаватель')
     begin = models.DateTimeField('Начало')
     end = models.DateTimeField('Окончание')
     event_type = models.CharField('Тип события', max_length=30)
     description = models.TextField('Описание')
 
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
