c0df6c94280aa5c1bf085a64864e0fa30518733b
Volodya Leveryev <volodya.leveryev@gmail.com>
2018-03-13 17:20:57 +0000

    Исправлена ошибка в параметре LOGIN_URL. Добавлена кнопка выхода из системы

--- diss/diss/settings.py
+++ diss/diss/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-LOGIN_URL = '/static/'
+LOGIN_URL = '/login/'
 
 STATIC_URL = '/static/'
 
